3f0d492bcfefb5db9cc3a599963441122b60a62c
Emir920 <emir.is.real.me@gmail.com>
2025-11-23 17:40:12 +0300

    project 1

--- app.py/sayılar.py
+++ app.py/sayılar.py
@@ -3,3 +3,15 @@ for a in range(5,51,5):
 
 tekSayilar = [s for s in range(1,10,2)]
 print(tekSayilar)
+
+import random
+import turtle
+
+
+ku = random.randint(20,250)
+for a in range(4):
+    turtle.forward(ku)
+    turtle.right(90)
+
+
+input()
