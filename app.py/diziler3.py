4272688e06ed50fefeb25d3b8af8f18f2603dd28
Emir920 <emir.is.real.me@gmail.com>
2025-11-30 17:31:57 +0300

    project 1

---
+++ app.py/diziler3.py
@@ -0,0 +1,21 @@
+# çok boyutlu diziler ve elemalarına ulaşma
+yemekler = [
+    ["tarhana","mercimek"],
+    ["musakka","adana"],
+    ["sütlaç","revani"],
+]
+# print(yemekler)
+# print(yemekler[0])
+print(yemekler[0][1])
+
+
+yemekler1 = {
+    "corbalar" : ["tarhana","mercimek"],
+    "anayemekler" : ["musakka","adana"],
+    "tatlilar" : ["sütlaç","revani"],
+}
+
+
+# print(yemekler1)
+# print(yemekler1["anayemekler"])
+print(yemekler1["anayemekler"][1])
