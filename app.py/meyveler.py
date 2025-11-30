5e25092cdbd93d0f0b52e746827952aeadb1b240
Emir920 <emir.is.real.me@gmail.com>
2025-11-30 16:56:21 +0300

    project 1

---
+++ app.py/meyveler.py
@@ -0,0 +1,20 @@
+meyveler = ['Muz', 'Nar', 'Dut'] # list
+haftasonu = ("Cumartesi","Pazar") # tupple
+ogrenci1 = {
+    "adi" : "Emir",
+    "soyadi" : "KARA",
+    "numarasi" : 457
+    } # sözlük
+kullaniciTuru = {"admin","ogretmen","ogrenci"} # set
+
+
+print("Liste : ", meyveler)
+print("Tuple : ", haftasonu)
+print("Dictionary : ", ogrenci1)
+print("Set : ", kullaniciTuru)
+
+
+print("Liste tipi : ", type(meyveler))
+print("Tuple tipi : ", type(haftasonu))
+print("Dictionary tipi : ", type(ogrenci1))
+print("Set tipi : ", type(kullaniciTuru))
