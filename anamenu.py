0aac0715f5dd3b7e264da0ec030c8e56251a1591
Emir920 <emir.is.real.me@gmail.com>
2025-12-14 13:39:59 +0300

    proje 1

--- anamenu.py
+++ anamenu.py
@@ -35,9 +35,7 @@ def get_valid_choice():
             print(f"Beklenmeyen bir hata oluştu: {e}. Lütfen tekrar deneyin.")
 
 def anamenu():
-    """
-    Main menu function for ETKİN APP.
-    """
+ 
     while True:
         display_menu()
         secim = get_valid_choice()
